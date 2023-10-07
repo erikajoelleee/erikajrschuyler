25da4c9ea372d6d196145127de9e66d119d70184
Erika Roncal-Schuyler <Roncal.schuyler@gmail.com>
2023-10-07 12:38:23 -0400

    testing

--- main.js
+++ main.js
@@ -18,15 +18,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
-// Wait for the page to fully load
-document.addEventListener("DOMContentLoaded", function() {
-    // Find the Resume link by its ID
-    const resumeLink = document.getElementById("resume-link");
-
-    // Check if the link exists
-    if (resumeLink) {
-        // Trigger a click event on the link
-        resumeLink.click();
-    }
-});
